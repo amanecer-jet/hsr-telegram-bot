5150b104985bcd1a94cf255f06966eb7bb7205d7
amanecer-jet <88768532+amanecer-jet@users.noreply.github.com>
2025-06-11 04:47:10 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+# Конфиг для будущих расширений (например, поддержка других игр)
+class BotConfig:
+    SUPPORTED_GAMES = [
+        "Honkai: Star Rail"
+    ]
+    GITHUB_DATA_URLS = {
+        "Honkai: Star Rail": {
+            "characters": "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/index_new/ru/characters.json",
+            "relics": "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/index_new/ru/relics.json",
+            "light_cones": "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/index_new/ru/light_cones.json",
+            "relic_main_affixs": "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/index_new/ru/relic_main_affixs.json",
+            "relic_sub_affixs": "https://raw.githubusercontent.com/Mar-7th/StarRailRes/master/index_new/ru/relic_sub_affixs.json"
+        }
+    }
